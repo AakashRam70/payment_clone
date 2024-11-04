9bc9311676e0ec087045df04355cbbafccbee553
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-11-04 09:44:16 +0530

    adding config,js for jsonwebtoken

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+const JWT_SECRET = "Aakash";
+
+module.exports = JWT_SECRET;
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const mainRouter = require("./routes/index")
 const cors = require('cors');
-const json = require("jsonwebtoken");
 const app = express();
 
 app.use(mainRouter);
 app.use(express.json());
 app.use(cors());
 
-app.listen(3001, () => {
-    console.log("Serber is Running..")
+app.listen(3000, () => {
+    console.log("Server is Running..")
 })
