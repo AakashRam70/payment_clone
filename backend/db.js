a6537b7d02c6eba7ae9991d91d3589a22d87ddaf
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-11-04 14:03:55 +0530

    updating db.js

--- backend/db.js
+++ backend/db.js
@@ -9,9 +9,7 @@ mongoose
         console.log(err, "Connection Error..")
     })
 
-const schema = mongoose.schema;
-
-const userSchema = schema({
+const userSchema = new mongoose.schema({
     username: {
         type: String,
         require: true
@@ -32,6 +30,21 @@ const userSchema = schema({
 
 const modelSchema = mongoose.model("User", userSchema);
 
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model('Account', accountSchema);
+
 module.exports = {
-    modelSchema
+    modelSchema,
+    Account
 }
