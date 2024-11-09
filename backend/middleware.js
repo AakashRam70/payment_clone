7db084921c7c646812b56d8f2ff6c74d4c412a56
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-11-09 18:41:16 +0530

    updating middleware.js

--- backend/middleware.js
+++ backend/middleware.js
@@ -4,7 +4,7 @@ const authMiddleware = (req, res, next) => {
     const authHeader = req.headers.authorization;
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
         return res.status(401).json({
-            message: "No token provided"
+            message: "No token provided or invalid token"
         })
     }
     const token = authHeader.split(" ")[1];
